9a5bc25de2b69a016d7c69f3a160a708fd1781f5
FankySs <pomklaondra@gmail.com>
2023-12-05 19:31:25 +0100

    Final review

--- Tanks.cpp
+++ Tanks.cpp
@@ -1,34 +1,9 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <math.h>
-#include <time.h>
-#include <windows.h>
-#include <conio.h> // Pouze pro Windows!
-
-#define WIDTH 140
-#define HEIGHT 40
-#define MAX_PLAYERS 5
-#define M_PI 3.14159265358979323846
-#define GRAVITY 10
-#define TERRAIN_CHAR (char)178    
-#define BORDER_CHAR (char)219  
-#define AIR_CHAR (char)176
-#define TANK_CHAR 'T' 
-#define EXPLOSION_CHAR 'X' 
-#define RADIUS_OF_EXPLOSION 1
+﻿// Spolupracoval Ondřej Pomkla a Jíří Řeháček.
+// Kod je psán dle zadání VUT FEKT BPC-AUD AUDB-ZVUK předmět BPC-PC1T
 
 
 
-
-// Struktura reprezentující tank v hře
-typedef struct Tank {
-    char name[20];
-    int xPosition;
-    int yPosition;
-    bool isHit;
-} Tank;
+#include "Tanks.h"
 
 // Deklarace funkcí
 
--- Tanks.h
+++ Tanks.h
@@ -4,5 +4,32 @@
 #pragma once
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <windows.h>
+#include <conio.h> // Pouze pro Windows!
 
-// TODO: Zde odkažte na dodatečné hlavičky, které program vyžaduje.
+#define WIDTH 140
+#define HEIGHT 40
+#define MAX_PLAYERS 5
+#define M_PI 3.14159265358979323846
+#define GRAVITY 10
+#define TERRAIN_CHAR (char)178    
+#define BORDER_CHAR (char)219  
+#define AIR_CHAR (char)176
+#define TANK_CHAR 'T' 
+#define EXPLOSION_CHAR 'X' 
+#define RADIUS_OF_EXPLOSION 1
+
+// Struktura reprezentující tank v hře
+typedef struct Tank {
+    char name[20];
+    int xPosition;
+    int yPosition;
+    bool isHit;
+} Tank;
+
